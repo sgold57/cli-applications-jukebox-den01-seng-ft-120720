c0fe4641dc79b0381aa77428970e8de051c49c04
Sam Gold <sgold5757@gmail.com>
2020-12-06 21:16:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,10 +30,9 @@ end
 def play(array)
   puts "Please enter a song name or number:"
   song_choice = gets.strip
-  song_choice_int = song_choice.to_i
   if array.include?(song_choice)
     puts "Playing #{song_choice}"
-  elsif song_choice_int <= array.length
+  elsif song_choice.to_i <= array.length
     puts "Playing #{array[song_choice_int - 1]}"
   else
     puts "Invalid input, please try again"
