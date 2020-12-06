552e071ca647b79ec4ecfd75eea977b502141bad
Sam Gold <sgold5757@gmail.com>
2020-12-06 20:34:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(array)
   puts "Please enter a song name or number:"
   song_choice = gets.strip
   array.find do |track|
-    song_choice.to_i == track.(index + 1)
+    song_choice.to_i == (track.index) + 1
   end
   puts "Playing #{song_choice}"
 end
